4b87a319ea95cb9aa2fea15b5567586e96481bf2
George Ion <89georgeion@gmail.com>
2023-08-29 22:08:37 +0300

    work work work

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,8 @@
+import ProductTile from '@/components/Catalog/ProductTile';
 import Footer from '@/components/common/Footer';
 import Header from '@/components/common/Header';
 import HomeCatalogPreview from '@/components/home/HomeCatalogPreview';
+import HomeNewsletter from '@/components/home/HomeNewsletter';
 import Payment from '@/components/home/payment';
 
 export default function Home() {
@@ -20,10 +22,25 @@ export default function Home() {
         <div className="layout-grid">
           <HomeCatalogPreview></HomeCatalogPreview>
         </div>
-        <div className="layout-bootstrap">
-          <HomeCatalogPreview></HomeCatalogPreview>
+
+        {/* layout bootstrap style */}
+        <div className="container">
+          <ul className="row justify-content-between">
+            <li className="col-12 col-lg-3 mb-5">
+              <ProductTile></ProductTile>
+            </li>
+            <li className="col-12 col-lg-3 offset-lg-1 mb-5">
+              <ProductTile></ProductTile>
+            </li>
+            <li className="col-12 col-lg-3 offset-lg-1">
+              <ProductTile></ProductTile>
+            </li>
+          </ul>
         </div>
-        <Payment></Payment>{' '}
+
+        <HomeNewsletter></HomeNewsletter>
+
+        <Payment></Payment>
       </main>
 
       <footer className="footer">
